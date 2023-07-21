b462685c5f697e4bd5486015ef632049d7fe0e19
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2023-07-21 15:37:26 -0700

    Created java sample code

---
+++ java/README.md
@@ -0,0 +1,11 @@
+# Tableau Connected Apps Direct Trust JWT Generation (Java)
+
+## Prerequisites
+- [OpenJDK](https://www.azul.com/downloads/?package=jdk#zulu)
+- [Apache Maven](https://maven.apache.org/install.html)
+- (will be installed by `mvn package`) Nimbus Jose+JWT (https://connect2id.com/products/nimbus-jose-jwt/download)
+
+## Getting Started
+
+> mvn package
+> java -jar target\jwt-sample-1.0-SNAPSHOT.jar
---
+++ java/src/main/java/JWT.java
@@ -0,0 +1,70 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+public class JWT {
+
+    public static void main(String[] args) {
+
+        String jwt = generateJWT();
+        System.out.println("Generating JWT...");
+        System.out.println(jwt);
+    }
+
+    public static String generateJWT(){
+        // Replace the example values below (remove the brackets).
+        // Store secrets securely based on your team's best practices.
+        // See: https://help.tableau.com/current/online/en-us/connected_apps_direct.htm
+
+        String secretId = "[Tableau Connected App Direct Trust Secret ID]";
+        String secretValue = "[Tableau Connected App Direct Trust Secret Value]";
+        String clientId = "[Tableau Connected App Direct Trust Client ID]";
+        String username = "[Tableau Username]";
+        double tokenExpiryInMinutes = 1; // Max of 10 minutes.
+        Calendar expiry = Calendar.getInstance();
+        expiry.add(Calendar.MINUTE, (int)tokenExpiryInMinutes);
+
+        // Remove 'embed_authoring' scope if Authoring is not needed.
+        String[] scopes = {"tableau:views:embed", "tableau:views:embed_authoring" };
+
+        UUID myGeneratedId = UUID.randomUUID();
+
+        // JWT generation: any strings defined below here are hardcoded special values
+        // once built, it is immutable
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+        .audience("tableau")
+        .subject(username)
+        .expirationTime(expiry.getTime())
+        .issuer(clientId)
+        .issueTime(new Date())
+        .jwtID(myGeneratedId.toString())
+        .claim("scp", scopes)
+        // example user-defined claim
+        .claim("Region", "East")
+        .build();
+
+        String JWTValue;
+        try {
+            JWSSigner signer = new MACSigner(secretValue);
+            JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).keyID(secretId).customParam("iss", clientId).build();
+
+            SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+            signedJWT.sign(signer);
+            JWTValue = signedJWT.serialize();
+        } catch (JOSEException jEx) {
+            System.out.println("JWT signing failed: ");
+            System.out.println(jEx);
+            JWTValue = jEx.getCause().getClass().getSimpleName();
+        }
+        return JWTValue;
+    }
+
+}
